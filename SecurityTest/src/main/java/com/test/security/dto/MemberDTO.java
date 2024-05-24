ff1bc781af8bb0a33db423dfc6ed00203cd53694
zyoo-d <156043182+SXNGJUNHX@users.noreply.github.com>
2024-05-24 17:40:14 +0900

    update 24.05.24
    update 24.05.24

---
+++ SecurityTest/src/main/java/com/test/security/dto/MemberDTO.java
@@ -0,0 +1,27 @@
+package com.test.security.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+
+	private String memberid;
+	private String memberpw;
+	private String membername;
+	private String enabled;
+	private String email;
+	private String gender;
+	private String regdate;
+	
+	
+	private List<AuthDTO> authlist;
+	private String auth; //회원가입용 (1: 회원, 2: 관리자)
+	
+}
+
---
+++ SecurityTest/src/main/java/com/test/security/mapper/TestMapper.java
@@ -0,0 +1,11 @@
+package com.test.security.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TestMapper {
+
+	@Select("select sysdate from dual")
+	String time();
+	
+	
+}
